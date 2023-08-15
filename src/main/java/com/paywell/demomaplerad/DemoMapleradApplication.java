e6b8e0758b370d2ea28dc4c66b1cf21ac9abcfbe
bigboytimi <olutimiolowookere@icloud.com>
2023-08-15 21:21:42 +0100

    maplerad-integration

---
+++ src/main/java/com/paywell/demomaplerad/DemoMapleradApplication.java
@@ -0,0 +1,13 @@
+package com.paywell.demomaplerad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMapleradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMapleradApplication.class, args);
+    }
+
+}
