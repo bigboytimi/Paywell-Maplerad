9a5f29696671c761f660f768bf323d92f8afe809
bigboytimi <olutimiolowookere@icloud.com>
2023-08-20 16:03:06 +0100

    maplerad-integration

--- src/main/java/com/paywell/demomaplerad/controllers/AuthController.java
+++ src/main/java/com/paywell/demomaplerad/controllers/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
-    private final CustomerRepository customerRepository;
+
     private final SignupService signupService;
 
     @PostMapping("/signup")
